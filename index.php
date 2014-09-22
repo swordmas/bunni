0a7e92427d0a01cf4f93aeb8bbac39f3c6d95fde
Roy Zheng <roycraft3@gmail.com>
2014-09-22 00:31:16 +0000

    fixes

--- index.php
+++ index.php
@@ -31,29 +31,7 @@
 
     <body>    
         <div id="container" class="home"><!-- main container -->
-            <div id="header">
-            	<a href="portfolio.php"><div id="main_logo"></div><!-- end logo --></a>
-                <div id="nav_bar"><!-- navigation bar -->
-                    <!-- probably not optimized code but it'll do -->
-                        <div id="bar" class="n_bar1">
-                            <h3 id="home" class="active">HOME</h3> <!-- active no hyperlink -->
-                        </div><!-- end bar1 -->
-                        <div id="bar" class="n_bar2">
-                            <a href="portfolio.php"><h3 id="home">PORTFOLIO</h3></a>
-                        </div><!-- end bar1 -->
-                        <div id="bar" class="n_bar3">
-                            <a href="resume.php"><h3 id="home">RESUME</h3></a>
-                        </div><!-- end bar1 -->
-                        
-                        <div id="bar" class="n_bar4">
-                            <a href="/blog"><h3 id="home">BLOG</h3></a>
-                        </div><!-- end bar1 -->
-                        
-                        <div id="bar" class="n_bar5">
-                            <a href="contact.php"><h3 id="home">BIO|CONTACT</h3></a>
-                        </div><!-- end bar1 -->
-                </div> <!-- end navigation bar -->
-            </div> <!-- end header -->
+	  <?php include('includes/header.php');?>
             <div id="content">
               
               <div class="parallax-viewport" id="parallax">
