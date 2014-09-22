48523b04b9b746e21d8f455b632d9706d8e62faf
Chen Chen <chenchen.illustrations@gmail.com>
2014-09-21 18:12:41 -0700

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -3,13 +3,13 @@
                 <div id="nav_bar"><!-- navigation bar -->                
                     <!-- probably not optimized code but it'll do -->
                         <div id="n_bar1" class="bar">
-                            <a href="index.php"><h3 class="home">HOME</h3></a> 
+                            <a href="index.php"><h3 id="home">HOME</h3></a> 
                         </div><!-- end bar1 -->
                         <div id="n_bar2" class="bar">
-                            <a href="portfolio.php"><h3 class="home">PORTFOLIO</h3></a>
+                            <a href="portfolio.php"><h3 id="home">PORTFOLIO</h3></a>
                         </div><!-- end bar1 -->
                         <div id="n_bar3" class="bar">
-                            <a href="resume.php"><h3 class="home">RESUME</h3></a>
+                            <a href="resume.php"><h3 id="home">RESUME</h3></a>
                         </div><!-- end bar1 -->
                         
                         <!--
@@ -19,7 +19,7 @@
                         
                         </div>-->
                         <div id="n_bar4" class="bar">
-                            <a href="contact.php"><h3 class="home">BIO|CONTACT</h3></a>
+                            <a href="contact.php"><h3 id="home">BIO|CONTACT</h3></a>
                         </div><!-- end bar1 -->
                 </div> <!-- end navigation bar -->
             </div> <!-- end header -->
