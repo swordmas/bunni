bd57dd972d92267d0f3bdd14e4216e35b29528b6
Roy Zheng <roycraft3@gmail.com>
2014-09-22 01:13:30 +0000

    apply all of chens' changes

--- contact.php
+++ contact.php
@@ -41,62 +41,8 @@
     <body>
     
         <div id="container" class="home"><!-- main container -->
-        
-        	
-            <div id="header">
-            
-            
-            	<a href="portfolio.php"><div id="main_logo"></div><!-- end logo --></a>
-            
-        	
-                <div id="nav_bar"><!-- navigation bar -->
-                
-                    <!-- probably not optimized code but it'll do -->
-                    
-                        <div id="bar" class="n_bar1">
-                        
-                        
-                            <a href="index.php"><h3 id="home">HOME</h3></a>
-                            
-                        
-                        </div><!-- end bar1 -->
-                        
-                        <div id="bar" class="n_bar2">
-                        
-                        
-                            <a href="portfolio.php"><h3 id="home">PORTFOLIO</h3></a>
-                            
-                                               
-                        </div><!-- end bar1 -->
-                        
-                        <div id="bar" class="n_bar3">
-                        
-                        
-                            <a href="resume.php"><h3 id="home">RESUME</h3></a>
-                        
-                        </div><!-- end bar1 -->
-                        
-                        <div id="bar" class="n_bar4">
-                        
-                            <a href="/blog"><h3 id="home">BLOG</h3></a>
-                        
-                        </div><!-- end bar1 -->
-                        
-                        <div id="bar" class="n_bar5">
-                        
-                            <h3 id="home" class="active">BIO|CONTACT</h3>
-                        
-                        </div><!-- end bar1 -->
-            
-                
-            
-                </div> <!-- end navigation bar -->
-                
-                </div> <!-- end header -->
-                
-                
-            
-                <div id="content">
+<?php include('includes/header.php'); ?>
+<div id="content">
                 
                 	<div class="parallax-viewport" id="parallax">
         
--- resume.php
+++ resume.php
@@ -29,32 +29,7 @@
     <body>
     
         <div id="container" class="home"><!-- main container -->
-        
-        	
-            <div id="header">
-            	<a href="portfolio.php"><div id="main_logo"></div><!-- end logo --></a>
-                <div id="nav_bar"><!-- navigation bar -->                
-                    <!-- probably not optimized code but it'll do -->
-                        <div id="bar" class="n_bar1">
-                            <a href="index.php"><h3 id="home">HOME</h3></a> 
-                        </div><!-- end bar1 -->
-                        <div id="bar" class="n_bar2">
-                            <a href="portfolio.php"><h3 id="home">PORTFOLIO</h3></a>
-                        </div><!-- end bar1 -->
-                        <div id="bar" class="n_bar3">
-                            <h3 id="home" class="active">RESUME</h3>
-                        </div><!-- end bar1 -->
-                        <div id="bar" class="n_bar4">
-                            <a href="/blog"><h3 id="home">BLOG</h3></a>
-                        </div><!-- end bar1 -->
-                        <div id="bar" class="n_bar5">
-                            <a href="contact.php"><h3 id="home">BIO|CONTACT</h3></a>
-                        </div><!-- end bar1 -->
-                </div> <!-- end navigation bar -->
-            </div> <!-- end header -->
-            
-                
-            
+<?php include('includes/header.php');?>     	
                 <div id="content">
                 
                 	
